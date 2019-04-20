c350830e7b28da6a4eac78b357ad93bddb95c782
Wick <wickerdj@gmail.com>
2019-04-20 09:16:34 -0500

    attempt using a different library

---
+++ dht11_m.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/MichaelS11/go-dht"
+)
+
+func main() {
+	err := dht.HostInit()
+	if err != nil {
+		fmt.Println("HostInit error:", err)
+		return
+	}
+
+	dht, err := dht.NewDHT("GPIO4", dht.Fahrenheit, "dht11")
+	if err != nil {
+		fmt.Println("NewDHT error:", err)
+		return
+	}
+
+	humidity, temperature, err := dht.ReadRetry(11)
+	if err != nil {
+		fmt.Println("Read error:", err)
+		return
+	}
+
+	fmt.Printf("humidity: %v\n", humidity)
+	fmt.Printf("temperature: %v\n", temperature)
+}
